3123ea8787efeeb20165470875d1e1f817ad1502
sippinyotrippin <van.didenko@gmail.com>
2022-09-10 12:03:36 +0300

    PEP8 added 2 blank lines after function definition, changed -> str to -> None

--- 6_1.py
+++ 6_1.py
@@ -1,12 +1,12 @@
 # Написать функцию перевода десятичного числа в двоичное и обратно, без
 # использования функции int
-def int_bin(num: int) -> str:
+def int_bin(num: int) -> None:
     b = ''
     while num > 0:
         b = str(num % 2) + b
         num = num // 2
     return print(b)
 
+
 number10 = int(input('Введите число в десятичной системе исчисления для перевода в двоичную: '))
 int_bin(number10)
-
