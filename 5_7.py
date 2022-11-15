d3ad204f808b78f5e8e053fbe4d70f450791654b
sippinyotrippin <van.didenko@gmail.com>
2022-11-15 19:01:18 +0300

    ValueError: min() arg is an empty sequence

--- 5_7.py
+++ 5_7.py
@@ -1,9 +1,8 @@
 #  Дан список чисел, отсортировать его по возрастанию без использования sort и sorted
 
-unsorted_list = [3, 6, 9, 7, 4, 5, 8]
+unsorted_list = [10, 9, 7, 6, 8, 4, 5, 3, 1, 2]
 sorted_list = []
 while unsorted_list:
-    element = min(unsorted_list)
-    unsorted_list.remove(element)
-    sorted_list.append(element)
+    unsorted_list.remove(min(unsorted_list))
+    sorted_list.append(min(unsorted_list))
 print(sorted_list)
