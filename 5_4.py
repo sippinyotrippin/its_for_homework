0d82876bb067da6dc8b29e6ba067b1aecda7a712
sippinyotrippin <van.didenko@gmail.com>
2022-11-24 18:08:28 +0300

    ready!

--- 5_4.py
+++ 5_4.py
@@ -5,14 +5,13 @@ from math import factorial
 def pascal(n):
     row = []
     for i in range(n):
-        element = factorial(n) // (factorial(i) * factorial(n - i))
+        element = str(factorial(n) // (factorial(i) * factorial(n - i)))
         row.append(element)
-    row.append(1)
+    row.append('1')
     return row
 
 
 n_str = int(input('enter the deep of triangle: ')) + 1
 for j in range(n_str):
     line = pascal(j)
-    print(line)
-
+    print(' ' * (n_str-j), ' '.join(line))
