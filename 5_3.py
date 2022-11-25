7fb12422f5754a4a56e0f5aca06268e333b1f989
sippinyotrippin <van.didenko@gmail.com>
2022-11-25 08:53:49 +0300

    ready!

--- 5_3.py
+++ 5_3.py
@@ -1,7 +1,6 @@
 # Вывести четные числа от 2 до N по 5 в строку
-n = int(input())
-for i in range(2, n + 1):
-    i = str(i)
-    i = [i]*5
-    i = ' '.join(i)
-    print(i)
+
+n = int(input('enter N: '))  # 30
+my_list = [str(i) for i in range(2, n+1) if i % 2 == 0]
+for i in range(0, round(n / 9)+1):
+    print(' '.join(my_list[i*5:5*i + 5]))
