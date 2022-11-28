7fcf899e428dbc2818a84d9f629469ed0cc1c4b4
sippinyotrippin <van.didenko@gmail.com>
2022-11-28 18:54:29 +0300

    turned procedure code into function receives input city and returns result appropriates conditions

--- 6_8.py
+++ 6_8.py
@@ -1,12 +1,23 @@
 # Дан словарь, ключ - Название страны, значение - список городов, на вход
 # поступает город, необходимо сказать из какой он страны
 
+# Дан словарь, ключ - Название страны, значение - список городов, на вход
+# поступает город, необходимо сказать из какой он страны
+
 data = {
     'Russia': ['Moscow', 'Saint Petersburg', 'Omsk'],
     'Belarus': ['Minsk', 'Brest', 'Gomel'],
     'Ukraine': ['Kiev', 'Kharkiv', 'Lviv']
 }
-city = input('city: ')
-for key, val in data.items():
-    if city in val:
-        print(key)
+
+
+def city_country(city):
+    for key, val in data.items():
+        if city in val:
+            return key
+        else:
+            return 'no find country to required city'
+
+
+my_city = input('city: ')
+print(city_country(my_city))
