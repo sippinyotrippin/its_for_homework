324043584d0bf0ab922b15007898c093c9317522
sippinyotrippin <van.didenko@gmail.com>
2023-08-13 10:38:46 +0300

    changed users.sql to bot_users.sql to reach more explicitness

--- sql_quires1/bot_users.sql
+++ sql_quires1/bot_users.sql
@@ -2,4 +2,4 @@ CREATE TABLE IF NOT EXISTS users(
 id SMALLSERIAL PRIMARY KEY,
 is_blocked BOOLEAN default(false),
 balance FLOAT, language_id SMALLINT NOT NULL,
-FOREIGN KEY (language_id) REFERENCES languages(id)) ON DELETE CASCADE;
+FOREIGN KEY (language_id) REFERENCES languages(id) ON DELETE CASCADE;
